2b2987839a27a5f26ae5eafdc3e92e0e3c9448a4
Reence <david.reence@gmail.com>
2022-02-01 23:04:21 +0800

    Added setup

--- app/src/views/Pages/Dashboard.vue
+++ app/src/views/Pages/Dashboard.vue
@@ -2,6 +2,21 @@
     <section>
         <h2>Dashboard</h2>
         <img src="@/assets/logo.png" alt="">
+        Article ID: {{  article.id  }} <br>
+        Article Title: {{  article.title  }} <br>
+        Article Excerpt: {{  article.excerpt  }}
+
+        <br>
+        <br>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <p>Articles</p>
+            <ul>
+                <li v-for="article in articles.data" :key="article.id">
+                    {{ article.title }}
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
@@ -12,17 +27,44 @@ export default {
 </script>
 
 <script setup>
-import { inject, onMounted } from "vue"
+import { inject, onMounted, ref, reactive } from "vue"
+
+const initArticle = {
+    id: null,
+    title: "",
+    excerpt: ""
+}
+const loading = ref(true)
+const article = reactive({ ...initArticle })
+const articles = reactive({
+    data: []
+})
 
 const api = inject("$api")
 
 const getArticle = async () => {
-    const response = await api.get("articles/8")
-
-    console.log(response.data)
+    try {
+        const response = await api.get("articles/8");
+        Object.assign(article, response)
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+const getArticles = async () => {
+    try {
+        const response = await api.get("testoci");
+        loading.value = false
+        Object.assign(articles, response)
+    } catch (error) {
+        loading.value = false
+        console.log(error)
+    }
+}
+
 onMounted(async () => {
     await getArticle()
+    await getArticles()
 })
 </script>
 
